e89784d01fee20b20ed16e61066c8ddda6de5757
Nataliia-Homyk324 <homyk324@gmail.com>
2024-09-24 23:38:38 +0300

    delete rem

--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -1,54 +1,10 @@
-// import { useEffect, useState } from "react";
-// import { getDatabase, ref, get } from "firebase/database";
-// import TeacherCard from "../TeacherCard/TeacherCard.jsx";
-// import Loader from "../Loader/Loader.jsx";
-
-// const TeachersList = () => {
-//   const [teachers, setTeachers] = useState([]);
-
-//   useEffect(() => {
-//     const fetchTeachers = async () => {
-//       const db = getDatabase();
-//       const teachersRef = ref(db, "teachers");
-//       const snapshot = await get(teachersRef);
-
-//       if (snapshot.exists()) {
-//         const data = snapshot.val();
-//         const teachersArray = Object.values(data);
-//         setTeachers(teachersArray);
-//       } else {
-//         console.log("Дані відсутні.");
-//       }
-//     };
-
-//     fetchTeachers().catch(console.error);
-//   }, []);
-
-//   return (
-//     <div>
-//       {teachers.length > 0 ? (
-//         teachers.map((teacher) => (
-//           <TeacherCard
-//             key={`${teacher.name}_${teacher.surname}`}
-//             teacher={teacher}
-//           />
-//         ))
-//       ) : (
-//         <Loader />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TeachersList;
-
 import { useEffect, useState } from "react";
 import { getDatabase, ref, get } from "firebase/database";
 import TeacherCard from "../TeacherCard/TeacherCard.jsx";
 import Loader from "../Loader/Loader.jsx";
-import { app } from "../../../firebase.js"; // Importing the Firebase app
+import { app } from "../../../firebase.js";
 
-const database = getDatabase(app); // Using Firebase app to get the database instance
+const database = getDatabase(app);
 
 const TeachersList = () => {
   const [teachers, setTeachers] = useState([]);
@@ -75,10 +31,7 @@ const TeachersList = () => {
     <div>
       {teachers.length > 0 ? (
         teachers.map((teacher) => (
-          <TeacherCard
-            key={teacher.id} // Use the id provided in the teacher object
-            teacher={teacher} // Pass the teacher object with the id
-          />
+          <TeacherCard key={teacher.id} teacher={teacher} />
         ))
       ) : (
         <Loader />
